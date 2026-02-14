3d673f491ed27b53a4e7775977ec099cd7a7adc8
westabdu <nihaikapibukucu@gmail.com>
2026-02-14 21:19:36 +0300

    fix: database.js'ye all fonksiyonu eklendi

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,5 +1,6 @@
+// src/utils/database.js
 import User from "../models/User.js";
-import Guild from "../models/Guild.js"; // Loto gibi sunucu verileri için
+import Guild from "../models/Guild.js";
 
 const db = {
     get: async (key) => {
@@ -19,11 +20,52 @@ const db = {
     set: async (key, value) => {
         if (key.startsWith("stats_")) {
             const [_, guildId, userId] = key.split("_");
-            return await User.findOneAndUpdate({ guildId, userId }, value, { upsert: true, new: true });
+            return await User.findOneAndUpdate(
+                { guildId, userId }, 
+                value, 
+                { upsert: true, new: true }
+            );
         }
         if (key.startsWith("lotto_")) {
             const guildId = key.split("_")[1];
-            return await Guild.findOneAndUpdate({ guildId }, { lotto: value }, { upsert: true, new: true });
+            return await Guild.findOneAndUpdate(
+                { guildId }, 
+                { lotto: value }, 
+                { upsert: true, new: true }
+            );
+        }
+    },
+
+    // ✨ YENİ: all() fonksiyonu
+    all: async () => {
+        const result = [];
+        
+        // Tüm kullanıcıları getir
+        const users = await User.find({});
+        users.forEach(user => {
+            result.push({
+                id: `stats_${user.guildId}_${user.userId}`,
+                data: user.toObject()
+            });
+        });
+        
+        // Loto verilerini de ekleyebilirsin (istersen)
+        
+        return result;
+    },
+
+    // ✨ YENİ: delete fonksiyonu (istersen)
+    delete: async (key) => {
+        if (key.startsWith("stats_")) {
+            const [_, guildId, userId] = key.split("_");
+            return await User.deleteOne({ guildId, userId });
+        }
+        if (key.startsWith("lotto_")) {
+            const guildId = key.split("_")[1];
+            return await Guild.updateOne(
+                { guildId },
+                { $unset: { lotto: 1 } }
+            );
         }
     }
 };
